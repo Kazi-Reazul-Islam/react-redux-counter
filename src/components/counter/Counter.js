61ec1f5b9b342ab74fe49adbed51a82e76ac0647
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-01-16 22:16:54 +0600

    done

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useRef} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment, setCustom} from "../../redux/state/counter/counterSlice";
 
 const Counter = () => {
+    const myNumber = useRef()
+  const count = useSelector((state)=>state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div className="card">
+    <div className="card text-center">
       <div className="card-header bg-secondary">
         <h4 className="text-white ">My Counter App</h4>
       </div>
       <div className="card-body">
-        <h4>00</h4>
+        <h4>{count}</h4>
         <div className="my-4">
-          <button className="btn btn-success">Increase</button>
-          <button className="btn btn-danger mx-2">Decrease</button>
+          <button onClick={()=>{dispatch(increment())}} className="btn btn-success">Increase</button>
+          <button onClick={()=>{dispatch(decrement())}} className="btn btn-danger mx-2" disabled={count === 0}>Decrease</button>
         </div>
+          <div className="my-4">
+              <input ref={myNumber} className="form-control " type="number" />
+              <button onClick={()=>{dispatch(setCustom(myNumber.current.value))}} className="btn btn-danger my-4 ">Set Custom</button>
+          </div>
       </div>
     </div>
   );
